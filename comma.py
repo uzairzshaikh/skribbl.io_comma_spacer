fdfde2658370701d78f86b5a6e828e8e7224f7e9
Uzair <uzair.z.shaikh@gmail.com>
2021-01-25 20:43:56 +0000

    Initial commit

---
+++ comma.py
@@ -0,0 +1,36 @@
+
+
+
+
+sentinel = ""
+print('Paste your text separated by new line.')
+word = ','.join(iter(input, sentinel))
+print(word)
+
+
+
+### test material below ###
+
+"""   Captain America: The First Avenger (takes place during WWII)
+    Captain Marvel (takes place in 1995)
+    Iron Man (takes place in 2010)
+    Iron Man 2 (takes place after Iron Man)
+    The Incredible Hulk (time unspecified, pre-Avengers)
+    Thor (takes place six months before Avengers)
+    The Avengers (takes place in 2012)
+    Iron Man 3 (takes place six months after The Avengers)
+    Thor: Dark World (post-Avengers, pre-Ultron)
+    Captain America: Winter Soldier (post-Avengers, pre-Ultron)
+    Guardians of the Galaxy (sometime in 2014)
+    Guardians of the Galaxy Vol. 2 (after Guardians)
+    Avengers: Age of Ultron (takes place in 2015)
+    Ant-Man (takes place in 2015)
+    Captain America: Civil War (post-Ultron, pre-Infinity War)
+    Spider-Man: Homecoming (post-Civil War, pre-Infinity War)
+    Doctor Strange (takes place in 2016)
+    Black Panther (takes place in 2017)
+    Thor: Ragnarok (post-Ultron, pre-Infinity War)
+    Avengers: Infinity War (takes place in 2017)
+    Ant-Man and The Wasp (ambiguous, but fits nicely between IW and Endgame)
+    Avengers: Endgame (starts in 2017, finishes in 2022)
+    Spider-Man: Far From Home (post-Endgame)"""
